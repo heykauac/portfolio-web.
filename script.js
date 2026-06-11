e92a4572dc0ccaf812aa4ebbac518dc21bb355a2
Kauã Cardoso <ckaua4692@gmail.com>
2026-06-11 02:01:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-
 const btnToggle = document.getElementById('theme-toggle');
 const body = document.body;
 
 if (localStorage.getItem('theme') === 'light-theme') {
-    body.classList.add('dark-theme'); 
+    body.classList.add('dark-theme');
     btnToggle.textContent = '☀️';
 } else {
- 
     btnToggle.textContent = '🌓';
 }
 
@@ -21,22 +19,6 @@ btnToggle.addEventListener('click', () => {
         btnToggle.textContent = '🌓';
     }
 });
-
-
-const typingSpan = document.getElementById('typing-sub-js');
-const textToType = "um Analista de Cibersegurança em Construção | Dev | Gamer";
-let index = 0;
-
-function typeText() {
-    if (index < textToType.length) {
-        typingSpan.textContent += textToType.charAt(index);
-        index++;
-        setTimeout(typeText, 70); 
-    }
-}
-
-document.addEventListener('DOMContentLoaded', typeText);
-
 
 const spanAno = document.querySelectorAll('#ano-atual');
 const anoAtual = new Date().getFullYear();
